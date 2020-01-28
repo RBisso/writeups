d1274b88f2a29c2f47a7059e0c02e31b771c878c
Rodrigo Bisso <rodrigobissomachado@gmail.com>
2020-01-27 20:26:01 -0500

    Add files via upload

---
+++ traverxec-htb/script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cat /home/david/bin/server-stats.head
+echo "Load: `/usr/bin/uptime`"
+echo " "
+echo "Open nhttpd sockets: `/usr/bin/ss -H sport = 80 | /usr/bin/wc -l`"
+echo "Files in the docroot: `/usr/bin/find /var/nostromo/htdocs/ | /usr/bin/wc -l`"
+echo " "
+echo "Last 5 journal log lines:"
+/usr/bin/sudo /usr/bin/journalctl -n5 -unostromo.service 
+less /etc/profile
+
